e8ebc3f90983c032f599bd560797fbe4032b6d0b
Pere Villega <pere.villega@gmail.com>
2013-10-12 20:06:03 +0100

    Add I18N
    Add config for I18N
    Add sample message to test I18N use

--- src/main/java/com/perevillega/eia/HelloController.java
+++ src/main/java/com/perevillega/eia/HelloController.java
@@ -2,10 +2,14 @@ package com.perevillega.eia;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Locale;
 
 @Controller
 @RequestMapping("/")
@@ -13,9 +17,13 @@ public class HelloController {
 
     private Logger log = LoggerFactory.getLogger(getClass());
 
+    @Autowired
+    MessageSource messageSource;
+
     @RequestMapping(method = RequestMethod.GET)
-    public String printWelcome(ModelMap model) {
-        model.addAttribute("message", "Hello world!");
+    public String printWelcome(ModelMap model, Locale locale) {
+        String msg = messageSource.getMessage("home.title", null, locale);
+        model.addAttribute("message", msg);
         log.debug("Sample logging entry to test it works");
         return "hello";
     }
